f89618e959b676757ce252950fb8bb1db340347e
menelik worku <menelikworku@meneliks-MacBook-Pro.local>
2022-03-01 10:26:16 +0100

    Finished part two of the Gatsby tutorial

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <Layout pageTitle="Contact">
+        <p>This page tells you a little how to contact me...</p>
+      </Layout>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/pages/products.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+class Products extends React.Component {
+  render() {
+    return (
+      <Layout pageTitle="Products">
+        <p>This is my products page.</p>
+      </Layout>
+    );
+  }
+}
+
+export default Products;
